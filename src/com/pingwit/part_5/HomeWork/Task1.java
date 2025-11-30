eac41cbd6b339229570e06496f3c67cbaa9f4ef4
Valera <valera.bejan@gmail.com>
2025-11-30 21:02:36 +0100

    HomeWork -part5

---
+++ src/com/pingwit/part_5/HomeWork/Task1.java
@@ -0,0 +1,12 @@
+package com.pingwit.part_5.HomeWork;
+
+public class Task1 {
+    static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+         //   if ( i % 2 != 0) {
+            if ( i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/com/pingwit/part_5/HomeWork/Task3.java
@@ -0,0 +1,58 @@
+package com.pingwit.part_5.HomeWork;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите размер бабочки: ");
+        int n = sc.nextInt();
+
+        // Верхняя часть
+        for (int i = 1; i <= n; i++) {
+
+            // Левое крыло
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Пробелы
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Правое крыло
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Нижняя часть
+        for (int i = n; i >= 1; i--) {
+
+            // Левое крыло
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Пробелы
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Правое крыло
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
